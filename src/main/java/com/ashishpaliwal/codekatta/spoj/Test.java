1d2a9438e22ffbad5b451bdee76cc4a28915c861
paliwalashish <paliwalashish@gmail.com>
2013-03-11 16:36:24 +0530

    addes spoj problem

---
+++ src/main/java/com/ashishpaliwal/codekatta/spoj/Test.java
@@ -0,0 +1,17 @@
+package com.ashishpaliwal.codekatta.spoj;
+
+import java.util.Scanner;
+
+/**
+ * For Problem TEST
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        while((input = scanner.nextInt()) != 42) {
+            System.out.println(input);
+        }
+    }
+}

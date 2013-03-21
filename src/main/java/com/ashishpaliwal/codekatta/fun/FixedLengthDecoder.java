55fa5dcf7ec6909f50b4114d11b55389d8c1d1d6
paliwalashish <paliwalashish@gmail.com>
2013-03-21 19:19:05 +0530

    playing with Decoder

---
+++ src/main/java/com/ashishpaliwal/codekatta/fun/FixedLengthDecoder.java
@@ -0,0 +1,45 @@
+package com.ashishpaliwal.codekatta.fun;
+
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+
+/**
+ * Fixed length decoder
+ */
+public abstract class FixedLengthDecoder<M> {
+
+    private ByteBuffer messageBuffer;
+    private int messageLength;
+    private volatile boolean lengthDecoded;
+
+    public M decode(ByteBuffer byteBuffer) {
+
+        // check for length
+        if(messageBuffer == null) {
+            messageLength = getLength(byteBuffer);
+            messageBuffer = ByteBuffer.allocate(messageLength);
+            if(byteBuffer.remaining() >= messageLength) {
+                messageBuffer.put(byteBuffer.array(), 0, messageLength);
+            }
+        }
+
+
+        return null;
+
+    }
+
+    protected abstract int getLength(ByteBuffer byteBuffer);
+
+    protected abstract M doDecode(ByteBuffer messageBuffer);
+
+    private void copyBuffers(ByteBuffer sourceBuffer, ByteBuffer destinationBuffer, int srcIndex, int length) {
+        if(destinationBuffer.capacity() < length) {
+            throw new BufferOverflowException();
+        }
+        sourceBuffer.position(srcIndex);
+        for (int i = srcIndex; i < srcIndex + length; i++) {
+             destinationBuffer.put(sourceBuffer.get());
+        }
+    }
+
+}

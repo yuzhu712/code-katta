aa0a1f2f8c0cbd952843f69c3619b2b391d89bf0
paliwalashish <paliwalashish@gmail.com>
2013-04-05 14:37:22 +0530

    added generic sample

--- src/main/java/com/ashishpaliwal/codekatta/avro/AvroGettingStarted.java
+++ src/main/java/com/ashishpaliwal/codekatta/avro/AvroGettingStarted.java
@@ -1,7 +1,12 @@
 package com.ashishpaliwal.codekatta.avro;
 
+import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumReader;
@@ -9,6 +14,8 @@ import org.apache.avro.specific.SpecificDatumWriter;
 
 import java.io.File;
 import java.io.IOException;
+
+import static org.apache.avro.Schema.Parser;
 
 /**
  * Sample to play with avro
@@ -38,5 +45,30 @@ public class AvroGettingStarted {
             user2 = fileReader.next();
             System.out.println(user2);
         }
+
+        // Without generating the Schema
+        Schema schema = new Parser().parse(AvroGettingStarted.class.getClassLoader().getResourceAsStream("user.avsc"));
+
+        GenericRecord userGr1 = new GenericData.Record(schema);
+        userGr1.put("name", "genericUser");
+        userGr1.put("favorite_number", 51);
+
+        System.out.println("Writing Generic file record");
+        File file = new File("genericRecordUser.avro");
+        DatumWriter<GenericRecord> datumWriter1 = new GenericDatumWriter<GenericRecord>(schema);
+        DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWriter1);
+        dataFileWriter.create(schema, file);
+        dataFileWriter.append(userGr1);
+        dataFileWriter.close();
+
+        System.out.println("Reading Generic File Record");
+        DatumReader<GenericRecord> datumReader1 = new GenericDatumReader<GenericRecord>(schema);
+        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(file, datumReader1);
+        GenericRecord genericRecord = null;
+        while (dataFileReader.hasNext()) {
+            genericRecord = dataFileReader.next();
+            System.out.println(genericRecord);
+        }
+
     }
 }

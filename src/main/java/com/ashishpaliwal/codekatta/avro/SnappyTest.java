2a56b6583ba78d5a829d7678f393595d7f4699dd
paliwalashish <paliwalashish@gmail.com>
2013-11-18 18:50:20 +0530

    added file

---
+++ src/main/java/com/ashishpaliwal/codekatta/avro/SnappyTest.java
@@ -0,0 +1,19 @@
+package com.ashishpaliwal.codekatta.avro;
+
+import org.xerial.snappy.Snappy;
+
+/**
+ *
+ */
+public class SnappyTest {
+
+    public static void main(String[] args) throws Exception {
+        String input = "Hello snappy-java! Snappy-java is a JNI-based wrapper of "
+                + "Snappy, a fast compresser/decompresser.";
+        byte[] compressed = Snappy.compress(input.getBytes("UTF-8"));
+        byte[] uncompressed = Snappy.uncompress(compressed);
+
+        String result = new String(uncompressed, "UTF-8");
+        System.out.println(result);
+    }
+}
